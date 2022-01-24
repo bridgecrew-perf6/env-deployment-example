3dfe4d8a3127508f540496d89a01ec258e71661f
hermanlyx <61788143+hurrmun@users.noreply.github.com>
2022-01-24 11:44:31 +0800

    added example

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <h2>secret env: {secret}</h2>
+      <h2>secret env: {secret ? secret : "secret not found"}</h2>
     </div>
   );
 }
