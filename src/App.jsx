8cd73f39c22676c83e028b954423d9b03d26917c
hermanlyx <61788143+hurrmun@users.noreply.github.com>
2022-01-24 11:32:43 +0800

    started testing

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import logo from "./logo.svg";
+import "./App.css";
+// import "dotenv/config";
+
+function App() {
+  // console.log(process.env);
+  const secret = process.env.REACT_APP_SECRET;
+
+  console.log(secret);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h2>secret env: {secret}</h2>
+    </div>
+  );
+}
+
+export default App;
